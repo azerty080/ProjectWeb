6a7ad6f4ed2e4ebc1e9f646ddd4d5c9732094e32
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-04-23 13:55:14 +0200

    commited changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 const appRoutes: Routes = [
 	{ path: '', component: HomeComponent, pathMatch: 'full' },
 	{ path: 'login', component: LoginComponent, pathMatch: 'full' },
+	{ path: 'contact', component: ContactComponent, pathMatch: 'full' },
 	// { path: '**', component: PageNotFoundComponent }
 ]
 
