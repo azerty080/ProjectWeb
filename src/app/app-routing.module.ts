4f10979ef5b28a47172be4b20f08f1b9792e438d
Joorren <Jorendandois@hotmail.com>
2018-05-17 12:26:13 +0200

    Left Nav Routing Works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,25 @@ import { StellingenComponent } from './stellingen/stellingen.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { AuthGuard } from './guards/auth.guard';
+import {SeksueleIntimidatieComponent} from './home/home-body/seksuele-intimidatie/seksuele-intimidatie.component';
+import {StrafbaarComponent} from './home/home-body/strafbaar/strafbaar.component';
+import {AangifteComponent} from './home/home-body/aangifte/aangifte.component';
+import {SlachtofferComponent} from './zelf-doen/zelf-doen-body/slachtoffer/slachtoffer.component';
+import {GetuigeComponent} from './zelf-doen/zelf-doen-body/getuige/getuige.component';
+import {PlegerComponent} from './zelf-doen/zelf-doen-body/pleger/pleger.component';
 
 const appRoutes: Routes = [
-	{ path: '', redirectTo: 'wat-is-seksuele-intimidatie', pathMatch: 'full' },
-	{ path: 'wat-is-seksuele-intimidatie', component: HomeComponent, pathMatch: 'full' },
+	{ path: '', redirectTo: 'wat-is-seksuele-intimidatie/wat-is-seksuele-intimidatie', pathMatch: 'full' },
+	{ path: 'wat-is-seksuele-intimidatie', redirectTo: 'wat-is-seksuele-intimidatie/wat-is-seksuele-intimidatie', pathMatch: 'full' },
+	{ path: 'wat-is-seksuele-intimidatie/wat-is-seksuele-intimidatie', component: HomeComponent, pathMatch: 'full', children: [{path: '', component: SeksueleIntimidatieComponent, outlet: 'content'}] },
+	{ path: 'wat-is-seksuele-intimidatie/welk-gedrag-is-strafbaar', component: HomeComponent, pathMatch: 'full', children: [{path: '', component: StrafbaarComponent, outlet: 'content'}] },
+	{ path: 'wat-is-seksuele-intimidatie/hoe-kan-ik-aangifte-doen', component: HomeComponent, pathMatch: 'full', children: [{path: '', component: AangifteComponent, outlet: 'content'}] },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
 	{ path: 'wat-kan-je-zelf-doen', component: ZelfDoenComponent, pathMatch: 'full' },
+	{ path: 'wat-kan-je-zelf-doen/als-slachtoffer', component: ZelfDoenComponent, pathMatch: 'full', children: [{path: '', component: SlachtofferComponent, outlet: 'content'}] },
+	{ path: 'wat-kan-je-zelf-doen/als-getuige', component: ZelfDoenComponent, pathMatch: 'full', children: [{path: '', component: GetuigeComponent, outlet: 'content'}] },
+	{ path: 'wat-kan-je-zelf-doen/als-pleger', component: ZelfDoenComponent, pathMatch: 'full', children: [{path: '', component: PlegerComponent, outlet: 'content'}] },
 	{ path: 'stellingen', component: StellingenComponent, pathMatch: 'full' },
   { path: 'contact', component: ContactComponent, pathMatch: 'full' },
   { path: 'user', loadChildren: './user/user.module#UserModule', canActivate: [AuthGuard] },
