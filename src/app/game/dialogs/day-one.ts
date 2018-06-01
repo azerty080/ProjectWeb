e78a8e198e01b225315d798ce166006f3717dfac
LazloDB <lazlodebock@hotmail.com>
2018-06-01 15:50:44 +0200

    game pt 1

---
+++ src/app/game/dialogs/day-one.ts
@@ -0,0 +1,36 @@
+export const scene1 = [
+  { dialog: "Ugh, het is terug maandag. Waarom moet school zo vroeg beginnen?",
+    by: "mc" },
+  { dialog: "NAAM, kom je naar beneden? Ontbijt is klaar en het is bijna tijd voor school.",
+    by: "mom" },
+  { dialog: "Ben direct beneden, ik moet me nog aankleden.",
+    by: "mc" },
+  { dialog: "Goeiemorgen Mama.",
+    by: "mc" },
+  { dialog: "Goeiemorgen  schat, goed geslapen?",
+    by: "mom" },
+  { dialog: "hmmm, uhu.",
+    by: "mc" },
+  { dialog: "Wel het is bijna tijd, ga je rugzak maar halen en dan kan ja naar school gaan.",
+    by: "mom" },
+];
+
+export const scene2 = [
+  { dialog: "MC zit in de klas met nog 12 andere kinderen, de leerkracht is namen aan het afroepen. De leerkracht roept jouw naam en je antwoordt: “Ben er.”",
+    by: "narrator" },
+  { dialog: "De leerkracht gaat de rest van de namen af en je zit maar naar buiten te staren.",
+    by: "narrator" },
+  { dialog: "Net buiten de schoolpoort zie je een jongen die tegen de muur wordt geduwd door twee grotere jongens.",
+    by: "narrator"},
+  { dialog: "Net buiten de schoolpoort zie je een jongen die tegen de muur wordt geduwd door twee grotere jongens.",
+    by: "narrator"},
+  { dialog: "De grotere jongens lachen en lopen weg en de kleinere jongen zit met zijn rug tegen de muur, tranen lopen van zijn wangen.",
+    by: "narrator"},
+];
+
+export const scene3 = [
+  { dialog: "De hele klas loopt op de speelplaats en iedereen verdeelt zich in groepjes, jij en je vrienden staan in een groep en beginnen te praten en lachen.",
+    by: "narrator" },
+  { dialog: "Je ziet de jongen die tegen de muur werd geduwd.",
+    by: "narrator" },
+];
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { scene1, scene2, scene3 } from './dialogs/day-one';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  text: String;
+  counter: any = 0;
+  scene: any = 1;
+  canvasElement: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.text = scene1[0].dialog;
+    this.canvasElement = document.getElementById('canvas');
   }
 
+  next() {
+    if (this.scene == 1 && this.counter < scene1.length - 1) {
+      this.counter = this.counter + 1;
+      this.text = scene1[this.counter].dialog;
+      return;
+    } else if (this.scene == 1) {
+      this.resetCounter();
+      this.newScene();
+    }
+
+    if (this.counter == 0 && this.scene == 2) {
+      this.canvasElement.classList.add('classroom');
+    }
+
+    if (this.scene == 2 && this.counter < scene2.length) {
+      this.text = scene2[this.counter].dialog;
+      this.counter = this.counter + 1;
+      return;
+    } else if (this.scene == 2 && this.counter == scene2.length) {
+      this.resetCounter();
+      this.newScene();
+    }
+
+    if (this.counter == 0 && this.scene == 3) {
+      this.canvasElement.classList.add('playground');
+    }
+
+    if (this.scene == 3 && this.counter < scene3.length) {
+      this.text = scene3[this.counter].dialog;
+      this.counter = this.counter + 1;
+      return;
+    }
+  }
+
+  resetCounter() {
+    this.counter = 0;
+  }
+
+  newScene() {
+    this.scene = this.scene + 1;
+  }
 }
